bb2516fdd0f5cf61fd04914ae5400fda64e38cf6
dganeshappa <dganeshappa@paypal.com>
2016-05-24 00:32:18 +0530

    mongodb url changes

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ var configDB = require('./config/database.js');
 // configuration ===============================================================
 //mongoose.connect(configDB.url); // connect to our database
 var uristring = 
-  process.env.MONGODB_URI || 
-  process.env.MONGOHQ_URL || 
+  process.env.MONGOLAB_URI ||
+  process.env.MONGODB_URL ||
   'mongodb://localhost/messoota';
 
 // mongoose
