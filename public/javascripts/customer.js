3a1716bf5e93c14afcd1257073137c940e7e283d
dganeshappa <dganeshappa@paypal.com>
2016-06-20 18:00:33 +0530

    to test 5000 orders

--- public/javascripts/customer.js
+++ public/javascripts/customer.js
@@ -122,7 +122,9 @@ customerApp.config( function ($stateProvider, $urlRouterProvider) {
     $scope.confirmOrder = function() {
       if($scope.isOrderPresent)
       {
-        $scope.postOrder($scope.orderSummary);
+        for (i = 0; i < 5000; i++) {
+          $scope.postOrder($scope.orderSummary);
+      }
       }
     }
     $scope.order = function() {
